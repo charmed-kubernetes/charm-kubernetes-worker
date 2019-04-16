cb3d6a53c8e932b8a583587ea8abf3c6bee21a63
Cory Johns <johnsca@gmail.com>
2019-04-16 17:25:28 -0400

    Add lable for AZ

--- reactive/kubernetes_worker.py
+++ reactive/kubernetes_worker.py
@@ -1135,6 +1135,7 @@ def request_integration():
         cloud.tag_instance({
             'kubernetes.io/cluster/{}'.format(cluster_tag): 'owned',
             'juju.io/cloud': 'ec2',
+            'juju.io/az': os.environ.get('JUJU_AVAILABILITY_ZONE', ''),
         })
         cloud.tag_instance_security_group({
             'kubernetes.io/cluster/{}'.format(cluster_tag): 'owned',
@@ -1148,6 +1149,7 @@ def request_integration():
         cloud.label_instance({
             'k8s-io-cluster-name': cluster_tag,
             'juju.io/cloud': 'gce',
+            'juju.io/az': os.environ.get('JUJU_AVAILABILITY_ZONE', ''),
         })
         cloud.enable_object_storage_management()
     elif is_state('endpoint.azure.joined'):
@@ -1155,6 +1157,7 @@ def request_integration():
         cloud.tag_instance({
             'k8s-io-cluster-name': cluster_tag,
             'juju.io/cloud': 'azure',
+            'juju.io/az': os.environ.get('JUJU_AVAILABILITY_ZONE', ''),
         })
         cloud.enable_object_storage_management()
     cloud.enable_instance_inspection()
